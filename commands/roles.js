63a0a9cf215968f271f4e235efe95150022a78eb
XtratoS <mamdouh.waleed.96@gmail.com>
2019-04-22 05:13:28 +0200

    Removed credentials
    Removed credentials and added a `message link` field to the command

--- commands/roles.js
+++ commands/roles.js
@@ -1,7 +1,8 @@
 const Discord = require("discord.js")
 const reaction_message_model = require("../models/reaction_roles_message.js")
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://XtratoS:UfTMXyIAxTN6Xr3q@nbot-dg4ad.mongodb.net/rbot?retryWrites=true', { useNewUrlParser: true })
+const conf = require('../conf.js');
+mongoose.connect(conf.mongodbs, { useNewUrlParser: true })
 
 /**
  * Shows all the roles on all the messages in the server
@@ -19,6 +20,7 @@ module.exports = async (bot, message, args) => {
             .addField("More info", "Shows all the roles on all the messages in the server.")
         return message.channel.send(help).then(r => r.delete(30000))
     }
+    let channels = message.guild.channels
     // find all the roles that can be added with reacting on any message in this server
     reaction_message_model.find({
         guildID: message.guild.id
@@ -31,13 +33,13 @@ module.exports = async (bot, message, args) => {
                 .addField("Empty", "No reaction roles found in this server, use the command **addemo help** to figure out how to add roles on reacting to a message.")
             return message.channel.send(norec).then(r => r.delete(60000))
         }
-        let channels = message.guild.channels
         // loop through all the found records, find the channel in which the message exists, then create a placeholder richembed
-        records.forEach(record => {
-            channel_obj = channels.find(c => c.id == record.channelID)
+        records.forEach(async record => {
+            channel_obj = await channels.find(c => c.id == record.channelID)
+            message_obj = await channel_obj.fetchMessage(record.messageID)
             let record_embed = new Discord.RichEmbed()
                 .setColor("#F0FFFA")
-                //.addField("Message Channel", channel_obj)
+                .setDescription(`[**Message Link**](https://discordapp.com/channels/${message.guild.id}/${channel_obj.id}/${message_obj.id})\n`)
             for (emote in record.roles) {
                 // add each emoji and the corresponding role to the richembed
                 record_embed.setDescription((record_embed.description || "")+"**—**"+emote+"**—"+record.roles[emote]["name"]+"—**\n")
