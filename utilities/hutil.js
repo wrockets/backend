63b8ef1dede31bc6c91547a32ee09f8b48ca38f2
XtratoS <mamdouh.waleed.96@gmail.com>
2019-05-01 08:59:25 +0200

    Removed Unnecessary functions
    Removed Unnecessary functions
    
    .

--- utilities/hutil.js
+++ utilities/hutil.js
@@ -1,10 +1,10 @@
 module.exports = {
     // return elements of an array in a string, side by side, separated by ', '
-    printarrayh(arrayy){
+    printarrayh(arr){
         output = ''
         var i
-        for(i=0, l=arrayy.length; i<l; i++){
-            output += arrayy[i] + ', '
+        for(i=0, l=arr.length; i<l; i++){
+            output += arr[i] + ', '
         }
         return output.slice(0, -2)
     },
@@ -18,33 +18,5 @@ module.exports = {
                 return false;
         }
         return true;
-    },
-    mystrify(word){
-        var L = word.length;
-        var i
-        let output=''
-        for(i=0; i<L; i++){
-            if(word.charAt(i) == ' ')
-                output += ' '
-            else
-                output += '❓'
-        }
-        return output
-    },
-    // reveals letters from the mystrified word
-    unmystrify(mystrified, l, normal){
-        var L = normal.length;
-        var i
-        let output = ''
-        for(i=0; i<L; i++){
-            let ctc = normal.charAt(i)
-            if(l == ctc){
-                output += ctc
-            }
-            else{
-                output += mystrified.charAt(i)
-            }
-        }
-        return output
     }
 }
