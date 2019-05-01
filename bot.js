982a2b973fc3b1344a9118f723a278dbbd9799a1
XtratoS <mamdouh.waleed.96@gmail.com>
2019-05-01 09:02:15 +0200

    Modified according to last review check desc.
    Changes:
    -added semicolons.
    -changed long functions from arrow notation to normal notation.
    -fixed some indenation and formatting mistakes.
    -args now has the command name as the first argument, also changed all command files accordingly, to start working with arguments from index 1 instead of 0.
    -moved all requirments to the top of the files.
    
    TODO:
    -use json files pseudo-database instead of mongodb
    
    .

--- bot.js
+++ bot.js
@@ -1,33 +1,30 @@
-const Discord  = require('discord.js');
-const conf     = require('./conf.js');
-const DT       = require('luxon').DateTime;
+const Discord = require('discord.js');
+const conf = require('./conf.js');
+const DT = require('luxon').DateTime;
 const commands = require('./commands.js');
-const Args     = require('string-argv');
+const Args = require('string-argv');
+const reaction_bot = require("./reaction_roles.js");
 
 const client = new Discord.Client();
 
-client.on('ready', function() {
+client.on('ready', function () {
 	console.log(`Logged in as ${client.user.tag}!`);
 });
 
-// reaction roles code
-const reaction_bot = require("./reaction_roles.js");
 // check every event for reaction add/remove on the reaction role message.
-client.on("raw", event => {
-    const event_type = event.t;
-    if ((event_type === "MESSAGE_REACTION_ADD" || event_type === "MESSAGE_REACTION_REMOVE")) {
-        reaction_bot.run(client, event).catch(err => console.log(err));
+client.on("raw", function (event) {
+	const event_type = event.t;
+	if ((event_type === "MESSAGE_REACTION_ADD" || event_type === "MESSAGE_REACTION_REMOVE")) {
+		reaction_bot.run(client, event).catch(err => console.log(err));
 	}
 })
 
-client.on('message', function (msg){
+client.on('message', function (msg) {
 	if (msg.content.startsWith(conf.commandPrefix)) {
 		var args = Args(msg.content);
 		var cmd = args[0].substring(conf.commandPrefix.length);
-		args.shift();
 		if (commands[cmd])
 			commands[cmd](client, msg, args);
-
 		else
 			msg.reply("Sorry, I don't know that command");
 	}
