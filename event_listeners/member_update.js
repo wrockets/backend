02c80cb9c96556d998924a7e963c54e2fa056b14
XtratoS <mamdouh.waleed.96@gmail.com>
2019-05-13 19:32:33 +0200

    Fixed member update event listener
    Fixed member update event listener ( logs role updates and nickname changes )

--- event_listeners/member_update.js
+++ event_listeners/member_update.js
@@ -1,68 +1,57 @@
 const Discord = require('discord.js');
 const log = require('../actions/log.js');
+
+/**
+ * Returns the difference between the two array (elements that exist in only one of the arrays)
+ * @param {number[]} x 
+ * @param {number[]} y 
+ * @returns {number[]} the elements that exist in only one of the arrays
+ */
+function compare_array(x, y) {
+	var a = y.filter(e => !x.includes(e))
+	if (a.length > 0)
+		return a
+	return x.filter(e => !y.includes(e))
+}
+
 module.exports = function (bot) {
 	bot.on("guildMemberUpdate", async function (old, newm) {
-		var act = await old.guild.fetchAuditLogs({ limit: 1 });
-		act = act.entries.first();
-		if (!act) {
-			console.log("error");
-			return;
-		}
-		let executor = act.executor
-		let target = act.target
-		let action = act.changes[0].key.replace(/\W/g, '')
-		let action_data = ''
-		if (action === 'add' || action === 'remove') {
-			action_data = `**ACTION:** Role ${action}\n**ROLE:** <@&${act.changes[0].new[0].id}>\n`;
-		}
-		else if (action === 'nick') {
-			action_data = `**ACTION:** Nickname Change\n**FROM:** `
-			if (act.changes[0].old)
-				action_data += `${act.changes[0].old}`
+		var executor, action_data = '';
+		// date object placeholder
+		var rn = new Date();
+		// fetch Audit Logs
+		let audit_logs = old.guild.fetchAuditLogs({ limit: 5 });
+		// compares the roles of the old and new user to get the differnces in roles
+		var difference = compare_array(old.roles.map(r => r.id), newm.roles.map(r => r.id));
+		// check if the number of roles changed
+		if (difference.length > 0) {
+			let temp;
+			if (old.roles.array().length > newm.roles.array().length)
+				temp = 'remove';
 			else
-				action_data += `${target.username}`
-			action_data += `\u2003\u2003**TO:** `
-			if (act.changes[0].new)
-				action_data += `${act.changes[0].new}\n`
-			else
-				action_data += `${target.username}\n`
+				temp = 'add';
+			action_data += `**ACTION:** ${temp} role\n`;
+			difference.forEach(function(r){
+				action_data += `**ROLE ${difference.indexOf(r)+1}:** <@&${r}>\n`;
+			})
+			audit_logs = await audit_logs;
+			audit_log_entry = audit_logs.entries.filter(e => e.action === 'MEMBER_ROLE_UPDATE').first();
+			executor = audit_log_entry.executor;
+			if(executor.bot){
+				return
+			}
 		}
-		else{
-			console.log(action)
+		else if (old.displayName != newm.displayName) {
+			audit_logs = await audit_logs
+			executor = audit_logs.entries.filter(e => e.action === 'MEMBER_UPDATE').first().executor;
+			action_data += `**ACTION:** Username Change\n**FROM:** ${old.displayName}\u2003\u2003**TO:** ${newm.displayName}\n`;
 		}
-		//role = bot.guilds.find(g => g.id === old.guild.id).roles.find(r => r.name === role)
+		else {
+			return
+		}
 		let response = new Discord.RichEmbed()
 			.setColor("#00FFFF")
-			.setDescription(`${action_data}**EXECUTOR:** ${executor}\n**TARGET USER:** ${target}\n`);
-		log(bot, old.guild.id, response)
+			.setDescription(`${action_data}**EXECUTOR:** ${executor} (${executor.id})\n**TARGET USER:** ${old.user} (${old.user.id})\n**DATE/TIME: **${rn.getUTCFullYear()}-${rn.getUTCMonth()+1}-${rn.getUTCDay()}, ${rn.getUTCHours()}:${rn.getUTCMinutes()}:${rn.getUTCSeconds()}`);
+		log(bot, old.guild.id, response, 'member_update');
 	})
 }
-//**ACTION:** Role ${action}\n**ROLE:** <@&${role.id}>\n
-/*
-GuildAuditLogsEntry {
-  targetType: 'USER',
-  actionType: 'UPDATE',
-  action: 'MEMBER_ROLE_UPDATE',
-  reason: null,
-  executor:
-   User {
-     id: '298518538958667786',
-     username: 'XtratoS',
-     discriminator: '2159',
-     avatar: 'b7bb4638fe6415b597bce2ff5b0076a4',
-     bot: false,
-     lastMessageID: null,
-     lastMessage: null },
-  changes: [ { key: '$remove', old: undefined, new: [Array] } ],
-  id: '574454448584130561',
-  extra: null,
-  target:
-   User {
-     id: '298518538958667786',
-     username: 'XtratoS',
-     discriminator: '2159',
-     avatar: 'b7bb4638fe6415b597bce2ff5b0076a4',
-     bot: false,
-     lastMessageID: null,
-     lastMessage: null } }
-*/
